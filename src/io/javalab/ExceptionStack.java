21552e5e50e62e4c78ccfdd5c3688bdaefbd548d
Priyobrato Das <priyodas12@gmail.com>
2021-02-09 11:59:29 +0530

    4th commit

--- src/io/javalab/ExceptionStack.java
+++ src/io/javalab/ExceptionStack.java
@@ -1,7 +1,18 @@
 package io.javalab;
 
 public class ExceptionStack {
-    public static void main(String[] args) {
 
+    private static void doThis(){
+        System.out.println("do This");
+        doThat();
+    }
+
+    private static void doThat() {
+        System.out.println("do That");
+        System.out.println(10/0);
+    }
+
+    public static void main(String[] args) {
+        doThis();
     }
 }
